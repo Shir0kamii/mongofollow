5d13e3f530af81d0364cbad7094ac7d35292bf3a
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2017-06-16 23:05:31 +0200

    Setup collection for tests

--- test_mongofollow.py
+++ test_mongofollow.py
@@ -1,2 +1,21 @@
-def test_dumb():
-    pass
+import mongomock
+import pytest
+
+
+@pytest.fixture
+def clean_collection():
+    collection = mongomock.MongoClient().test.test
+    collection.drop()
+    return collection
+
+
+@pytest.fixture
+def collection_range100(clean_collection):
+    for i in range(100):
+        clean_collection.insert({"i": i})
+    return clean_collection
+
+
+def test_inserted(collection_range100):
+    assert collection_range100.count() == 100
+    assert set(range(100)) == set(collection_range100.distinct("i"))
