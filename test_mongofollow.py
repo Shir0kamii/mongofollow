500b2a1cc835c09ce0cd8003f611fda82dccd654
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2017-06-16 23:20:56 +0200

    Add test for mongofollow

--- test_mongofollow.py
+++ test_mongofollow.py
@@ -1,6 +1,8 @@
 import mongomock
 import pytest
 
+from mongofollow import mongofollow
+
 
 @pytest.fixture
 def clean_collection():
@@ -19,3 +21,14 @@ def collection_range100(clean_collection):
 def test_inserted(collection_range100):
     assert collection_range100.count() == 100
     assert set(range(100)) == set(collection_range100.distinct("i"))
+
+
+@pytest.mark.xfail(reason="to fix")
+def test_stream(clean_collection):
+    assert clean_collection.count() == 0
+    clean_collection.insert({"i": 0})
+    for i, doc in zip(range(10), mongofollow(clean_collection)):
+        print(i, doc)
+        j = doc["i"]
+        assert i == j
+        clean_collection.insert({"i": i + 1})
